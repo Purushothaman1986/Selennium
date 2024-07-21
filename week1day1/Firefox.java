abb4f23397d7c2f6d901c85acff95d18e99d4032
Purushothaman1986 <purusothaman1986@gmail.com>
2024-07-21 03:45:32 -0400

    Add files via upload
    Week-1

---
+++ week1day1/Firefox.java
@@ -0,0 +1,21 @@
+package week1day1;
+
+public class Firefox {
+	
+private static final float BrowserVersion = 100.2f;
+private static final String BrowsersName = "firefox";
+private static final char BrowserLogo = 'f';
+
+public static void main(String[] args) {
+	boolean isVisible =true;
+int releaseYear =1998;
+System .out .println("BrowserVersion :"+BrowserVersion);
+System .out .println("BrowserName : "+BrowsersName);
+System .out .println("ISVisible : "+isVisible);
+System .out .println("ReleaseYear : "+releaseYear);
+System .out .println("BrowserLogo : "+BrowserLogo);
+
+}
+
+}
+
